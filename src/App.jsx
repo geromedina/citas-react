a0a029819abe77a24747639163e782148a13a99e
geromedina <gerocode768@gmail.com>
2023-05-08 17:58:59 -0300

    todo listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
+import { useState, useEffect } from 'react';
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+import ListadoPacientes from "./components/ListadoPacientes"
+
 function App() {
 
+  const [pacientes, setPacientes ] = useState([]);
+  const [paciente, setPaciente] = useState({});
+
+  // Se ejecuta una sola vez, por eso le paso el array vacio
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS)
+    }
+    obtenerLS();
+  }, [])
+  
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ))
+  }, [pacientes])
+  
+
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
+    setPacientes(pacientesActualizados)
+  }
+
   return (
-    <div className='App'>
-      <h1>Hola mundo!</h1>
+    <div className="container mx-auto mt-20">
+
+      <Header />
+
+      <div className="mt-12 md:flex">
+        <Formulario 
+        pacientes={pacientes}
+        setPacientes={setPacientes}
+        paciente={paciente}
+        setPaciente={setPaciente}
+        />
+        <ListadoPacientes 
+        pacientes={pacientes}
+        setPaciente={setPaciente}
+        eliminarPaciente={eliminarPaciente}
+        />
+      </div>
+
     </div>
   )
 }
